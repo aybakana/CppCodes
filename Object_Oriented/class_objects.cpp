336d865b921908b4abbf51d3ed8e526d58404099
Abdulbaki <aybakana@itu.edu.tr>
2019-05-24 11:56:46 +0300

    Add files via upload

---
+++ Object_Oriented/class_objects.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+using namespace std;
+
+class Box{
+public:
+	double length;
+	double breath;
+	double height;
+};
+
+int main(){
+	Box box1;
+	Box box2;
+	double volume = 0.0;
+
+	box1.height = 5.0;
+	box1.length = 6.0;
+	box1.breath = 7.0;
+
+	box2.height = 10.0;
+	box2.length = 12.0;
+	box2.breath = 13.0;
+
+	volume = box1.height * box1.length * box1.breath;
+	cout << "Volume of box1: "<<volume<<endl;
+
+	volume = box2.height * box2.length * box2.breath;
+	cout << "Volume of box2: "<<volume<<endl;
+	return 0; 
+}
